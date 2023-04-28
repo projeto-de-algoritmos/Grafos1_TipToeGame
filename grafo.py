06b0bc6054dbacebaf67d8f2b25f8f9cd3518ea2
kessJhones <tavaresjhones14@gmail.com>
2023-04-28 15:53:43 -0300

    Aplicando camelCase

--- grafo.py
+++ grafo.py
@@ -5,34 +5,38 @@ class Graph:
         self.adj_matrix = [[0] * num_vertices for _ in range(num_vertices)]
         self.visited = [False] * num_vertices
     
-    def add_edge(self, v1, v2):
+    def addEdge(self, v1, v2):
         self.adj_matrix[v1][v2] = 1
         self.adj_matrix[v2][v1] = 1
     
-    def remove_edge(self, v1, v2):
+    def removeEdge(self, v1, v2):
         self.adj_matrix[v1][v2] = 0
         self.adj_matrix[v2][v1] = 0
     
-    def clear_visited(self):
+    def remove_all_edges(self, v):
+        for i in range(self.num_vertices):
+            self.removeEdge(v, i)
+    
+    def clearVisited(self):
         self.visited = [False] * self.num_vertices
     
     #tranforma matriz em grafo
-    def matrix_to_graph(self, matrix):
+    def matrixToGraph(self, matrix):
         element = 0
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if matrix[i][j] != 7:
                     # checa se é possivel ir para a direita, esquerda, cima ou baixo
                     if i+1 < len(matrix) and matrix[i+1][j] != 7:
-                        self.add_edge(element, len(matrix[i])+element)
+                        self.addEdge(element, len(matrix[i])+element)
                     if j+1 < len(matrix[i]) and matrix[i][j+1] != 7:
-                        self.add_edge(element, element+1)
+                        self.addEdge(element, element+1)
                     if i-1 >= 0 and matrix[i-1][j] != 7:
-                        self.add_edge(element, element-len(matrix[i]))
+                        self.addEdge(element, element-len(matrix[i]))
                     if j-1 >= 0 and matrix[i][j-1] != 7:
-                        self.add_edge(element, element-1)
+                        self.addEdge(element, element-1)
                 if matrix[i][j] != 7:
-                    self.add_edge(element, element)
+                    self.addEdge(element, element)
                 element += 1
 
     # cria caminho entre dois pontos usando busca em profundidade
@@ -48,34 +52,34 @@ class Graph:
         return None
     
     # transforma caminho em lista de coordenadas
-    def path_to_coordinates(self, path, matrix):
+    def pathToCoordinates(self, path, matrix):
         coordinates = []
         for i in path:
-            coordinates.append(self.index_to_coordinates(i, matrix))
+            coordinates.append(self.indexToCoordinates(i, matrix))
         return coordinates
     
     # transforma indice em coordenadas
-    def index_to_coordinates(self, index, matrix):
+    def indexToCoordinates(self, index, matrix):
         row = int(index) // len(matrix)
         column = index % len(matrix)
         return (row, column)
     
     # transforma coordenadas em indice
-    def coordinates_to_index(self, coordinates, matrix):
+    def coordinatesToIndex(self, coordinates, matrix):
         return coordinates[0]*len(matrix) + coordinates[1]
     
     # transforma caminho em lista de movimentos
-    def path_to_moves(self, path=[], matrix=[[]]):
+    def pathToMoves(self, path, matrix):
         moves = []
         if path != None:
             for i in range(len(path)-1):
-                moves.append(self.get_move(path[i], path[i+1], matrix))
+                moves.append(self.getMove(path[i], path[i+1], matrix))
         return moves
 
     # retorna movimento entre dois pontos
-    def get_move(self, start, end, matrix):
-        start = self.index_to_coordinates(start, matrix)
-        end = self.index_to_coordinates(end, matrix)
+    def getMove(self, start, end, matrix):
+        start = self.indexToCoordinates(start, matrix)
+        end = self.indexToCoordinates(end, matrix)
         if start[0] == end[0]:
             if start[1] > end[1]:
                 return [-1, 0]
